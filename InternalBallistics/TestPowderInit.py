bc3c2e2d46dd63542daa1b831ee0fdc946a5310b
sabbraxcaddabra <sabbraxcaddabra@yandex.ru>
2021-11-17 11:55:01 +0300

    В файле TestInitPowders сделал инициализацию и прверку порохов по конечному импульсу. На выходе получается кортеж кортежей со всеми возможными комбинациями порохов

---
+++ InternalBallistics/TestPowderInit.py
@@ -0,0 +1,33 @@
+from InternalBallistics.IntBalClasses import Powder
+from itertools import combinations
+
+def check_Jk(powder, Jk_dop, max_tol):
+    tol = abs((powder.Jk/Jk_dop)*100 - 100)
+    if tol <= max_tol:
+        return True
+
+def get_powder_combination(Jk_dop_list, max_tol=15.):
+    with open('PowdersBase.txt', encoding='utf8') as base_file:
+        non_powder = (
+            'Фл-тор',
+            'Инерт._добав.',
+            'Свинц._провол.'
+        )
+        powders_list = []
+        for line in base_file:
+            powder = Powder.from_data_string(line)
+            powder_check_list = [check_Jk(powder, Jk_dop, max_tol) for Jk_dop in Jk_dop_list]
+            if powder.name in non_powder or not any(powder_check_list):
+                continue
+            powders_list.append(powder)
+
+    comb_powders = tuple(combinations(powders_list, len(Jk_dop_list)))
+    return comb_powders
+
+
+if __name__ == "__main__":
+    Jk_dop_list = [343.8e3, 680e3]
+
+    comb = get_powder_combination(Jk_dop_list)
+
+
